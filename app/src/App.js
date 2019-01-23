4f489059ba98380bd83558c99de4840837ff2ccb
Jure Martinec <jure.martinec@gmail.com>
2019-01-23 17:40:32 +0100

    Set up mapbox

--- app/src/App.js
+++ app/src/App.js
@@ -1,25 +1,44 @@
 import React, { Component } from 'react';
+import MapGL, { Popup } from 'react-map-gl';
 import logo from './logo.svg';
 import './App.css';
 
+const MAPBOX_TOKEN = "pk.eyJ1IjoicmMzIiwiYSI6ImNlTWkwODAifQ.Ekx8PkIebxZdaZxksxz2BA";
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      mapStyle: "mapbox://styles/rc3/cjffxvy9wb3ou2snutp645m00/",
+      viewport: {
+        latitude: 38.88,
+        longitude: -98,
+        zoom: 3,
+        minZoom: 2,
+        bearing: 0,
+        pitch: 0
+      }
+    };
+
+    this.onViewportChange = this._onViewportChange.bind(this);
+  }
+  _onViewportChange(viewport) {
+    this.setState({ viewport });
+  }
   render() {
+    const { viewport, mapStyle } = this.state;
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <MapGL
+          {...viewport}
+          width="100%"
+          height="100%"
+          mapStyle={mapStyle}
+          mapboxApiAccessToken={MAPBOX_TOKEN}
+          onViewportChange={this.onViewportChange}
+        />
       </div>
     );
   }
